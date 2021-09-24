af1adf25032e1277603f31f163ae649de1fa5a19
Antonio Vendramini <antonio.vendramini@gft.com>
2021-09-24 20:14:39 +0200

    first commit

---
+++ src/main/java/com/exasol/challenge/pokerHands/exception/PokerHandException.java
@@ -0,0 +1,7 @@
+package com.exasol.challenge.pokerHands.exception;
+
+public class PokerHandException extends RuntimeException {
+    public PokerHandException(final String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/exasol/challenge/pokerHands/log/Log.java
@@ -0,0 +1,9 @@
+package com.exasol.challenge.pokerHands.log;
+
+public class Log {
+
+    static void info(String log) {
+        System.out.println(log);
+    }
+
+}
