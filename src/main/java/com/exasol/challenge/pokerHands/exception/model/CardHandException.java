92665d13a0a440b396e066269cee09e6813cd68a
Antonio Vendramini <antonio.vendramini@gft.com>
2021-09-26 11:30:52 +0200

    all examples works

---
+++ src/main/java/com/exasol/challenge/pokerHands/exception/model/CardHandException.java
@@ -0,0 +1,7 @@
+package com.exasol.challenge.pokerHands.exception.model;
+
+public class CardHandException extends RuntimeException {
+    public CardHandException(final String message){
+        super(message);
+    }
+}
